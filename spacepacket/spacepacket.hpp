9dad9f7c53dd3c805184326bfb65a73034f9ed2a
Alexis Cabana-Loriaux <cabana-loriaux@campus.tu-berlin.de>
2021-08-12 16:29:58 +0200

    extract base spacepacket class

--- spacepacket/spacepacket.hpp
+++ spacepacket/spacepacket.hpp
@@ -18,54 +18,34 @@ enum {
     SPACEPACKET_MAX_SIZE = 65542
 };
 
-/**
- * Spacepacket builder template
- */
-template<typename SecHdrType,
-        uint8_t IdleDataPattern = 0xFFU> // Pattern of Idle Data is set by the mission
-class SpBuilder : public Serializable
+template<typename SecHdrType>
+class ISpacepacket
 {
     static_assert((std::is_base_of<ISpSecondaryHeader, SecHdrType>::value), 
-                    "Secondary header type must be of type ISpSecondaryHeader");
+                "Secondary header type must be of type ISpSecondaryHeader");
 public:
-    SpBuilder(IBuffer& user_data_buffer)
-    : user_data(user_data_buffer) {
 
-    }
+    enum {
+        MIN_SIZE = 7,
+        MAX_SIZE = 65542
+    };
 
-    void serialize(OBitStream& o) const override {
-        // If present, the Packet Secondary Header shall follow, without gap, the Packet Primary Header (pink book,4.1.3.2.1.1)
-        // The user data field must also follow the secondary header (or primary header if no secondary header) (pink book, 4.1.3.3.1)
-        o << primary_hdr << secondary_hdr << user_data;
-    }
-    
-    std::size_t getSize() {
-        return SpPrimaryHeader::getSize() + SecHdrType::getSize() + user_data.getSize();
-    }
-
-    OBitStream& data() {
-        return user_data;
-    }
-
-    void fillIdleData(std::size_t nb_bytes) {
-        for(std::size_t i = 0; i < nb_bytes; i++) {
-            user_data.put(IdleDataPattern, CHAR_BIT);
-        }
-    }
+    virtual std::size_t getUserDataWidth() = 0;
 
     bool isValid() {
+        
         //There shall be a User Data Field, or a Packet Secondary Header, or both (pink book, 4.1.3.2.1.2 and 4.1.3.3.2)
-        if(SecHdrType::getSize() == 0 && user_data.getSize() == 0) {
+        if(SecHdrType::getSize() == 0 && this->getUserDataWidth() == 0) {
             return false;
         }
 
         //If present, the User Data Field shall consist of an integral number of octets (4.1.3.3.3)
-        if(user_data.getWidth() % CHAR_BIT != 0) {
+        if(this->getUserDataWidth() % CHAR_BIT != 0) {
             return false;
         }
 
         //A Space Packet shall consist of at least 7 and at most 65542 octets (pink book, 4.1.1.2)
-        if(this->getSize() < SPACEPACKET_MIN_SIZE || this->getSize() > SPACEPACKET_MAX_SIZE) {
+        if(this->getSize() < MIN_SIZE || this->getSize() > MAX_SIZE) {
             return false;
         }
 
@@ -93,8 +73,47 @@ public:
         return true;
     }
 
-    SpPrimaryHeader primary_hdr;
+     std::size_t getSize() {
+        return SpPrimaryHeader::getSize() + SecHdrType::getSize() + (this->getUserDataWidth() / CHAR_BIT) + 
+            (this->getUserDataWidth() % CHAR_BIT > 0 ? 1 : 0);
+     }
+
     SecHdrType      secondary_hdr;
+    SpPrimaryHeader primary_hdr;
+};
+
+/**
+ * Spacepacket builder template
+ */
+template<typename SecHdrType,
+        uint8_t IdleDataPattern = 0xFFU> // Pattern of Idle Data is set by the mission
+class SpBuilder : public ISpacepacket<SecHdrType>, public Serializable
+{
+public:
+    SpBuilder(IBuffer& user_data_buffer)
+    : user_data(user_data_buffer) {
+
+    }
+
+    void serialize(OBitStream& o) const override {
+        // If present, the Packet Secondary Header shall follow, without gap, the Packet Primary Header (pink book,4.1.3.2.1.1)
+        // The user data field must also follow the secondary header (or primary header if no secondary header) (pink book, 4.1.3.3.1)
+        o << this->primary_hdr << this->secondary_hdr << user_data;
+    }
+    
+    std::size_t getUserDataWidth() override {
+        return user_data.getWidth();
+    }
+
+    OBitStream& data() {
+        return user_data;
+    }
+
+    void fillIdleData(std::size_t nb_bytes) {
+        for(std::size_t i = 0; i < nb_bytes; i++) {
+            user_data.put(IdleDataPattern, CHAR_BIT);
+        }
+    }
 
 private:
     OBitStream      user_data;
@@ -105,8 +124,14 @@ private:
  */
 template<typename SecHdrType, 
         typename ...Fields>
-class SpDissector : public Deserializable
+class SpDissector : public ISpacepacket<SecHdrType>, public Deserializable
 {
+    static_assert((true && ... && std::is_base_of<IField, Fields>::value), 
+                    "RX Spacepacket fields must all derive from IField");
+    static_assert((0 + ... + Fields::getWidth()) % CHAR_BIT == 0, 
+                    "RX Spacepacket definition must fit in an integral number of octet");
+    static_assert(SecHdrType::getSize() > 0|| (0 + ... + Fields::getWidth()) > 0, 
+                    "There shall be a User Data Field, or a Packet Secondary Header, or both (pink book, 4.1.3.2.1.2 and 4.1.3.3.2)");
 public:
     SpDissector() 
     : is_pri_hdr_provided(false), is_sec_hdr_provided(false) {
@@ -114,29 +139,34 @@ public:
     }
 
     SpDissector(SpPrimaryHeader& pri_hdr)
-    : primary_hdr(pri_hdr), is_pri_hdr_provided(true), is_sec_hdr_provided(false) {
-
+    : is_pri_hdr_provided(true), is_sec_hdr_provided(false) {
+        this->primary_hdr = pri_hdr;
     }
 
     SpDissector(SpPrimaryHeader& pri_hdr, SecHdrType& sec_hdr)
-    : primary_hdr(pri_hdr), secondary_hdr(sec_hdr), is_pri_hdr_provided(true), is_sec_hdr_provided(true) {
-
+    : is_pri_hdr_provided(true), is_sec_hdr_provided(true) {
+        this->primary_hdr = pri_hdr;
+        this->secondary_hdr = sec_hdr;
     }
 
     void deserialize(IBitStream& i) override {
 
         // Only deserialize primary and secondary headers if they were not already provided at construction
         if(!is_pri_hdr_provided) {
-            i >> primary_hdr;
+            i >> this->primary_hdr;
             is_pri_hdr_provided = false;
         }
 
         if(!is_sec_hdr_provided) {
-            i >> secondary_hdr;
+            i >> this->secondary_hdr;
             is_sec_hdr_provided = false;
         }
 
         std::apply([&](auto&&... args){ (void(i >> args), ...); }, field_tuple);
+    }
+
+    std::size_t getUserDataWidth() override {
+        return (0 + ... + Fields::getWidth());
     }
 
     template<std::size_t index>
@@ -144,9 +174,6 @@ public:
         static_assert(index < (sizeof...(Fields) + 1), "Field index out of range");
         return std::get<index>(field_tuple);
     }
-
-    SpPrimaryHeader         primary_hdr;
-    SecHdrType              secondary_hdr;
 
 private:
     std::tuple<Fields...>   field_tuple;
