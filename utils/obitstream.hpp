6fa7721ec22882bd412d29765b245e3e96015a72
Alexis Cabana-Loriaux <cabana-loriaux@campus.tu-berlin.de>
2021-10-16 13:35:02 -0400

    Obitstream rework to MSB first

--- utils/obitstream.hpp
+++ utils/obitstream.hpp
@@ -20,38 +20,52 @@ public:
 
     }
 
+    /**
+     * 
+     */
     template<typename T>
     void put(T t, std::size_t width, bool isLittleEndian = false) {
+
+        (void)isLittleEndian;
+        std::size_t current_byte_i = this->cur_bit_offset / CHAR_BIT;
+        uint8_t* current_byte = buffer.getStart() + current_byte_i;
 
         if(bad_bit) {
             //invalid operation, can't use a bad stream
             return;
         }
 
-        if(width > sizeof(T)*CHAR_BIT) {
-            //invalid operation, can't put more bits than the amount given
+        if(width == 0) {
+            return;
+        }
+
+        if(width > sizeof(T)*CHAR_BIT || 
+           width > buffer.getSize()*CHAR_BIT - cur_bit_offset) {
+            //invalid operation, can't put any more bits
             bad_bit = true;
             return;
         }
-        
-        if(isLittleEndian) {
-            // least significant bits go first in the stream, per byte
-            while(width > 0) {
-                uint8_t nbBitsToAdd = MIN(width, CHAR_BIT); // take maximum 8 bits 
-                uint8_t mask = ONES(nbBitsToAdd);
-                this->putBits(t & mask, nbBitsToAdd);
-                t >>= nbBitsToAdd;
-                width -= nbBitsToAdd;
+
+        while(width > 0) {
+            // index in the current byte we should be writing from
+            uint8_t bit_index = CHAR_BIT - (cur_bit_offset % CHAR_BIT);
+
+            //clear current byte before write bits to it
+            if(cur_bit_offset % CHAR_BIT == 0) {
+                *current_byte = 0x00;
             }
-        } else {
-            // most significant bits go first in the stream
-            while(width > 0) {
-                uint8_t nbBitsToAdd = ((width - 1) % CHAR_BIT) + 1; // take minimum 1 bit 
-                uint8_t mask = ONES(nbBitsToAdd);
-                uint8_t byteNo = (width - 1) / CHAR_BIT;
-                this->putBits((t >> (byteNo * CHAR_BIT)) & mask, nbBitsToAdd);
-                width -= nbBitsToAdd;
-            }
+            
+            //mask relevant bits from value to put
+            uint8_t nbBitsToAdd = MIN(bit_index, width);
+            uint8_t value = (t >> (width - nbBitsToAdd)) & ONES(nbBitsToAdd);
+
+            //append relevant bits to current byte, at the right position
+            uint8_t shift = bit_index - nbBitsToAdd;
+            *current_byte |= (value << shift);
+
+            current_byte++;
+            width          -= nbBitsToAdd;
+            cur_bit_offset += nbBitsToAdd;
         }
     }
 
@@ -82,40 +96,17 @@ public:
             std::size_t nb_bits       = other.cur_bit_offset % CHAR_BIT;
             
             for(std::size_t i = 0; i < nb_full_bytes; i++) {
-                out.putBits(*(other.buffer.getStart() + i), CHAR_BIT);
+                out.put(*(other.buffer.getStart() + i), CHAR_BIT);
             }
 
             //might have remainder bits
             if(nb_bits > 0) {
-                out.putBits(*(other.buffer.getStart() + nb_full_bytes), nb_bits);
+                out.put(*(other.buffer.getStart() + nb_full_bytes), nb_bits);
             }
         }
         return out;
     }
 private:
-    void putBits(uint8_t byte, std::size_t width) {
-        
-        std::size_t byte_no = this->cur_bit_offset / CHAR_BIT;
-        std::size_t bit_no  = this->cur_bit_offset % CHAR_BIT;
-        uint8_t* cur_byte_ptr = this->buffer.getStart() + byte_no;
-        uint8_t value = byte & ONES(width);
-
-        // clear to all zeros (only bit positions we're about to write)
-        *cur_byte_ptr &= ONES(bit_no);
-        // append bits
-        *cur_byte_ptr |= value << bit_no;
-
-        // check if we need to do a second write because not all bits could be written
-        if((bit_no + width) > CHAR_BIT) {
-            //go to next byte
-            cur_byte_ptr++;
-
-            *cur_byte_ptr = 0;
-            *cur_byte_ptr |= value >> (CHAR_BIT - bit_no);
-        }
-
-        this->cur_bit_offset += width;
-    }
 
     //members
     IBuffer& buffer;
