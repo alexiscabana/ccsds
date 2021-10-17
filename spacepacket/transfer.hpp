ff07630f74972b478c426698d861df6fe22c1c66
Alexis Cabana-Loriaux <cabana-loriaux@campus.tu-berlin.de>
2021-10-17 18:42:25 -0400

    fix spacepacket transmission

--- spacepacket/transfer.hpp
+++ spacepacket/transfer.hpp
@@ -13,7 +13,7 @@ namespace ccsds
 class SpListener
 {
 public:
-    virtual void newSpacepacket() = 0;
+    virtual void newSpacepacket(IBuffer& bytes) = 0;
 };
 
 /**
@@ -47,11 +47,12 @@ class SpTransferService
     };
 
     struct ApidContext {
-        SpPrimaryHeader::SequenceCount next_count;
+        std::size_t rx_count = 0;
+        std::size_t tx_count = 0;
+        SpPrimaryHeader::SequenceCount next_count; //count is 0 by default
     };
 
     struct Telmetry {
-        std::size_t nb_idle = 0;
         ApidContext contexes[SpPrimaryHeader::PacketApid::IDLE_VALUE];
     };
 
@@ -65,15 +66,9 @@ public:
     }
 
     template<typename SecHdr>
-    void push(SpBuilder<SecHdr>& sp) {
-        IBitStream istream(sp.getBuffer());
-        SpPrimaryHeader pri_header;
-
-        istream >> pri_header;
-
-        if(pri_header.apid.isIdle()) {
-            telemetry.nb_idle++;
-        }
+    void transmit(SpBuilder<SecHdr>& sp) {
+        sp.finalize();
+        this->transmitBuffer(sp.getBuffer());
     }
 
     void registerListener(SpListener* listener) {
@@ -119,8 +114,32 @@ private:
         
     }
 
+    void transmitBuffer(IBuffer& buffer) {
+        IBitStream istream(buffer);
+        SpPrimaryHeader pri_header;
+
+        istream >> pri_header;
+
+        //update current context
+        auto apid_value = pri_header.apid.getValue();
+        ++contexes[apid_value].tx_count;
+        ++contexes[apid_value].next_count;
+
+        //listeners have to be notified of this new spacepacket
+        this->notifyListeners(pri_header.apid, buffer);
+    }
+
+    void notifyListeners(SpPrimaryHeader::PacketApid& apid, IBuffer& buffer) {
+        for(uint32_t i = 0; i < nb_listeners; i++) {
+            if(listenerEntries[i].matcher(apid)) {
+                listenerEntries[i].listener->newSpacepacket(buffer);
+            }
+        }
+    }
+
     std::size_t nb_listeners;
     ListenerEntry listenerEntries[LISTENERS_MAX_SIZE];
+    ApidContext contexes[SpPrimaryHeader::PacketApid::IDLE_VALUE];
     Telmetry telemetry;
 };
 
