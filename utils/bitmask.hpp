58d47db4daf3ceee4df8502b42027d64e66b39fa
Alexis Cabana-Loriaux <alex021994@gmail.com>
2021-10-25 11:45:50 -0400

    Add doxygen comments

--- utils/bitmask.hpp
+++ utils/bitmask.hpp
@@ -1,10 +1,25 @@
+/**************************************************************************//**
+ * @file bitmask.hpp
+ * @author Alexis Cabana-Loriaux
+ * 
+ * @brief Contains utilities for creating and manipulating bitmasks
+ * 
+ ******************************************************************************/
 #ifndef BITMASK_HPP
 #define BITMASK_HPP
 
 #include <climits>
 
+/**
+ * @brief Create a bitmask with a given amount of ones, starting from bit 0,
+ *        increasing in position
+ * 
+ * @param onecount The amount of ones in the bitmask
+ * 
+ * @return The bitmask
+ */
 template <typename R>
-static constexpr R bitmask(unsigned int const onecount)
+static constexpr R bitmask(const unsigned int onecount)
 {
     return static_cast<R>(-(onecount != 0))
         & (static_cast<R>(-1) >> ((sizeof(R) * CHAR_BIT) - onecount));
--- utils/buffer.hpp
+++ utils/buffer.hpp
@@ -1,13 +1,37 @@
+/**************************************************************************//**
+ * @file buffer.hpp
+ * @author Alexis Cabana-Loriaux
+ * 
+ * @brief Contains utilities for creating and manipulating buffers
+ *        and memory.
+ * 
+ ******************************************************************************/
 #ifndef BUFFER_HPP
 #define BUFFER_HPP
 
 #include "utils/printable.hpp"
 #include <cstdint>
 
+/**
+ * @brief Interface that abstracts a section of memory in the CCSDS environment.
+ *        IBuffers provides useful abstraction of memory so the user keeps full control
+ *        of the memory management, at all times.
+ */ 
 class IBuffer : public Printable
 {
 public:
+    /**
+     * @brief Get the start address of the buffer section
+     * 
+     * @return The start address
+     */
     virtual uint8_t*    getStart()       = 0;
+
+    /**
+     * @brief Get the size of the buffer section
+     * 
+     * @return The size
+     */
     virtual std::size_t getSize()  const = 0;
 
     void print() override {
@@ -19,7 +43,8 @@ public:
 };
 
 /**
- * 
+ * @brief Template class for a buffer of a given amount of bytes. 
+ *        WARNING: The memory is held inside the instance.
  */
 template<std::size_t Size>
 class Buffer : public IBuffer
@@ -37,15 +62,26 @@ public:
     }
 
 private:
+    /** The section of memory wrapped by this buffer instance */
     uint8_t bytes[Size] = { 0 };
 };
 
 /**
- * 
+ * @brief Template class for a buffer of a given amount of bytes, where
+ *        memory is held outside of this instance. UserBuffers are used to 
+ *        interact with the CCSDS environment while refering to memory already
+ *        managed by the user. Destroying this object does not release the memory
+ *        pointed to!
  */
 class UserBuffer : public IBuffer
 {
 public:
+    /**
+     * @brief Constructor
+     * 
+     * @param buffer the start address of the memory section to refer to
+     * @param max_size the size of the memory section to refer to
+     */
     UserBuffer(void* buffer, std::size_t max_size)
     : max_size(max_size), buf_start(static_cast<uint8_t*>(buffer)) {
 
@@ -60,7 +96,9 @@ public:
     }
     
 private:
+    /** The size of the memory section  */
     std::size_t max_size;
+    /** The start address of the memory section */
     uint8_t*    buf_start;
 };
 
